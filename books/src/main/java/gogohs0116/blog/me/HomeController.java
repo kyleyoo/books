ee092bb08377b2dedc2f7d3022da6ba526641c47
tpad <tpad@ADMIN>
2016-09-29 06:25:21 +0900

    first project

--- books/src/main/java/gogohs0116/blog/me/HomeController.java
+++ books/src/main/java/gogohs0116/blog/me/HomeController.java
@@ -33,6 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		System.out.println("asdasdasda");
+		System.out.println("sadjhaiosfhaof");
 		return "home";
 	}
 	
